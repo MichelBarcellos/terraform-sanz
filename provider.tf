5b3a376e8cfffcf57106dad2a61db2d487c1bfaf
Michel <michel@192.168.0.6>
2025-12-07 20:16:15 -0300

    atualizando nome da ec2

--- provider.tf
+++ provider.tf
@@ -11,3 +11,14 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+
+resource "aws_instance" "ec2-sanz" {
+  instance_type = "t3.micro"
+  ami           = "ami-02f3f602d23f1659d"
+  tags = {
+    ambiente = "dev"
+    Name = "ec2-sanz"
+  }
+  vpc_security_group_ids = [ "" ]
+}
